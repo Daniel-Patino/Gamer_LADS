13cecde7582d185cc324f8ed93661a80d5d16486
meiyaismywaifu <rokasomee@gmail.com>
2019-05-26 18:04:39 -0700

    pause menu resume functional

--- Assets/Scripts/PauseGame.cs
+++ Assets/Scripts/PauseGame.cs
@@ -2,19 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// for some reason this script does not work at canvas level.
 public class PauseGame : MonoBehaviour
 {
-    /*"If we want a variable to have the same value throughout all instances of the object then we can declare it as a static variable in our program. To manipulate and use the values of static variables we can also define a function as static.
+ /*"If we want a variable to have the same value throughout all instances of the object then we can declare it as a static variable in our program. To manipulate and use the values of static variables we can also define a function as static.
 
     The keyword "static" means that only one instance of a given variable exists for a class. Static variables are used to define constants because their values can be retrieved by invoking the class without creating an instance of it."*/
     public static bool PauseState = false;
-    public GameObject PauseScreen;
+    public GameObject PauseMenu;
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
+            Debug.Log("1");
             if (PauseState == true)
             {
                 Resume();
@@ -28,14 +30,16 @@ public class PauseGame : MonoBehaviour
 
     public void Resume()
     {
-        PauseScreen.SetActive(false);
+        Debug.Log("2");
+        PauseMenu.SetActive(false);
         Time.timeScale = 1f;
         PauseState = false;
     }
 
     void Pause()
     {
-        PauseScreen.SetActive(true);
+        Debug.Log("3");
+        PauseMenu.SetActive(true);
         Time.timeScale = 0f;
         PauseState = true;
     }
