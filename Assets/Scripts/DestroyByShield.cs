58a8ea8330a43a81af98d3785a2becd30b6489a5
Daniel-Patino <dp.automata@gmail.com>
2019-05-29 19:58:35 -0400

    More Shield Work

---
+++ Assets/Scripts/DestroyByShield.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByShield : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag != "Boundary")
+        {
+            if(other.tag != "Player")
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
--- Assets/Scripts/PlayerShieldController.cs
+++ Assets/Scripts/PlayerShieldController.cs
@@ -6,17 +6,19 @@ public class PlayerShieldController : MonoBehaviour
 {
     public GameObject playerObject;
     public int initialShieldLevel = 0;
+    public GameObject[] shields;
+
+    [HideInInspector] public int currentShieldLevel = 0;
 
     public void shieldUpdate()
     {
-        Debug.Log("Shield Updating");
-        shieldPowerUp();
+        Debug.Log("Shield Initialized");
+        Debug.Log("Shield Level: " + currentShieldLevel);
+        Instantiate(shields[currentShieldLevel - 1], playerObject.transform.position, Quaternion.Euler(90, 0, 0));
     }
 
     private void shieldPowerUp()
     {
         Debug.Log("Shield Updating 123");
     }
-
-    [HideInInspector] public int shieldLevel = 0;
 }
