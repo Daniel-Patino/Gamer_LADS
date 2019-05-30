6a80e52f2ffe30294a716a897d2b425fdeaed306
meiyaismywaifu <rokasomee@gmail.com>
2019-05-29 17:00:30 -0700

    game over screen setup

---
+++ Assets/Scripts/Menus/GameOverScreen.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverScreen : MonoBehaviour
+{
+    public GameObject GameOverDisplay;
+    public static bool GameOverState = false;
+    public PlayerRespawnController playerRespawnController;
+    public WaveScoreTextHandler waveScoreTextHandler;
+    public GameObject waveNumberText;
+    public GameObject scoreNumberText;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        GameOverState = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerRespawnController.lives <= 0)
+        {
+            GameOverState = true;
+        }
+
+        if (GameOverState == true)
+        {
+            GameOver();
+        }
+    }
+
+    private int wave;
+    private int score;
+    private void GameOver()
+    {
+        // waveText = waveScoreTextHandler.getCurrentScore;
+    
+    }
+    
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadSceneAsync("MainMenu");
+    }
+}
