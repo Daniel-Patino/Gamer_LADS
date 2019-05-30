1314934d26dd0e88fac502c40f7cdf89e62a8624
meiyaismywaifu <rokasomee@gmail.com>
2019-05-30 16:25:35 -0700

    partial game over screen
    w/ bunch of ways that don't work commented out.
    main problem: unity doesn't like multiple classes in a script.

--- Assets/Scripts/Menus/GameOverScreen.cs
+++ Assets/Scripts/Menus/GameOverScreen.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameOverScreen : MonoBehaviour
 {
     public GameObject GameOverDisplay;
     public static bool GameOverState = false;
     public PlayerRespawnController playerRespawnController;
-    public WaveScoreTextHandler waveScoreTextHandler;
+    // note: referencing class directly here requires inheriting from MonoBehaviour.
+    // inheriting MonoBehaviour has its own set of +/-. one is that it assumes attachment to GameObject.
+    public GameObject gameController;
     public GameObject waveNumberText;
     public GameObject scoreNumberText;
+    public int test;
 
     // Start is called before the first frame update
     void Awake()
@@ -30,14 +34,26 @@ public class GameOverScreen : MonoBehaviour
         {
             GameOver();
         }
+
+        testText = scoreNumberText.GetComponent<Text>();
+        testText.text = gameController.GetComponent<UserInterfaceController>().scoreCarrier().ToString();
+
+        // Debug.Log(gameController.GetComponent<UserInterfaceController>().scoreCarrier());
     }
 
     private int wave;
     private int score;
+    private Text testText;
+
     private void GameOver()
     {
+        // testText = waveNumberText.GetComponent<Text>();
+        // testText.text = waveScoreTextHandler.getCurrentScore().ToString();
+
+        ///= waveScoreTextHandler.getCurrentScore();
+
         // waveText = waveScoreTextHandler.getCurrentScore;
-    
+        // waveNumberText.Text = waveScoreTextHandler.getCurrentScore();
     }
     
 
