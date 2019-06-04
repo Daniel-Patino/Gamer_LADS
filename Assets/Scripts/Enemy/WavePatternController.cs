eb795cc06807e271d63b5e7afc0067b6c6431a25
meiyaismywaifu <rokasomee@gmail.com>
2019-06-04 16:56:45 -0700

    wavePattern / enemyMove initial

---
+++ Assets/Scripts/Enemy/WavePatternController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WavePatternController : MonoBehaviour
+{
+    public Transform[] waypointSet;
+    public GameObject Wave;
+    public int test = 0;
+
+    void Start()
+    {
+
+        waypointSet = new Transform[Wave.transform.childCount];
+        for (int i = 0; i < waypointSet.Length; i++)
+        {
+            waypointSet[i] = Wave.transform.GetChild(i);
+        }
+    }
+}
