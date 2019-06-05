bd505e6847dec5cae957cf3f9abc731e8e53a484
meiyaismywaifu <rokasomee@gmail.com>
2019-06-05 16:57:31 -0700

    WavePatternController work progress
    moved said class from WavePattern to EnemyController

---
+++ Assets/Scripts/Enemy/WaveEnemyCounts/EnemyCount_WP01.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCount_WP01 : MonoBehaviour
+{
+    public int enemyCount = 2;
+}
--- Assets/Scripts/Enemy/WavePatternController.cs
+++ Assets/Scripts/Enemy/WavePatternController.cs
@@ -6,15 +6,83 @@ public class WavePatternController : MonoBehaviour
 {
     public Transform[] waypointSet;
     public GameObject Wave;
-    public int test = 0;
 
-    void Start()
+    public UserInterfaceController userInterfaceController;
+    private int currentScore;
+    public MeteorSpawner meteorSpawner;
+    private int meteorRemainCount;
+
+    public float startWait = 5f;
+
+    bool initialGrace = true;
+
+    void Awake()
     {
-
+        // move this later
         waypointSet = new Transform[Wave.transform.childCount];
         for (int i = 0; i < waypointSet.Length; i++)
         {
             waypointSet[i] = Wave.transform.GetChild(i);
         }
+
+        // set up first wave stuff
+
+        initialGrace = true;
+        StartCoroutine(WaveControlMainMethod());
+        
+    }
+
+    /* initial timer disable everything
+     * when score exceed some value, spawn next wave.
+     * spawn enemies at certain time interval until wave count runs out
+     * 
+     * would be interesting if we could spawn multiple waves at once...
+     * 
+     * 
+     * 
+     */
+
+    void Update()
+    {
+        float timeElapsed = Time.timeSinceLevelLoad;        
+    }
+
+    private float interimTimer;
+    
+
+    void WaveControlMainMethod2()
+    {
+        // initial freedom from enemies
+        // this implementation is called a "flag".
+        while (initialGrace == true)
+        {
+            if (Time.timeSinceLevelLoad >= 5.0)
+            {
+                initialGrace = false;
+            }
+        }
+
+
+
+
+
+    }
+    
+    IEnumerator WaveControlMainMethod()
+    {
+        yield return new WaitForSeconds(startWait);
+        for (int i = 0; i < 5; i++) // while waves AND meteors have not been exhausted
+        {
+            if (currentScore == 2) // if score > some threshold
+            {
+                // begin spawning wave
+            }
+
+        }
+    }
+
+    IEnumerator Placeholder()
+    {
+        yield return new WaitForSeconds(0);
     }
 }
