ead9d66a8ed2ca2ff8160d8c0353ed0c9d154f7a
meiyaismywaifu <rokasomee@gmail.com>
2019-06-06 16:08:27 -0700

    WavePatternController progress 06_06

--- Assets/Scripts/Enemy/WavePatternController.cs
+++ Assets/Scripts/Enemy/WavePatternController.cs
@@ -4,31 +4,45 @@ using UnityEngine;
 
 public class WavePatternController : MonoBehaviour
 {
-    public Transform[] waypointSet;
-    public GameObject Wave;
+    // Unsorted Active
+    public GameObject WaveContainer;
+    private WaveReader waveReader;
 
+    // Unsorted Leftovers
+
+    // Readers
     public UserInterfaceController userInterfaceController;
     private int currentScore;
     public MeteorSpawner meteorSpawner;
     private int meteorRemainCount;
 
+    // Balance Variables
     public float startWait = 5f;
 
+    // Internal Use
     bool initialGrace = true;
+    public Transform[] waypointSet;
 
     void Awake()
     {
-        // move this later
-        waypointSet = new Transform[Wave.transform.childCount];
-        for (int i = 0; i < waypointSet.Length; i++)
-        {
-            waypointSet[i] = Wave.transform.GetChild(i);
-        }
+        //--- TEMP TEST ---
+        // waypoint objects to array
+        // waypointSet = new Transform[Wave.transform.childCount];
+        // for (int i = 0; i < waypointSet.Length; i++)
+        // {
+        //     waypointSet[i] = Wave.transform.GetChild(i);
+        // }
+
+        // reader/writer of waves
+        waveReader = new WaveReader(WaveContainer);
+        waveReader.test();
+
+
+        //--- TEMP TEST ---
 
         // set up first wave stuff
 
         initialGrace = true;
-        StartCoroutine(WaveControlMainMethod());
         
     }
 
@@ -50,7 +64,7 @@ public class WavePatternController : MonoBehaviour
     private float interimTimer;
     
 
-    void WaveControlMainMethod2()
+    void WaveControlMainMethod()
     {
         // initial freedom from enemies
         // this implementation is called a "flag".
@@ -62,27 +76,64 @@ public class WavePatternController : MonoBehaviour
             }
         }
 
+        // wouldnt it be interesting if you could have two waves at once
+        // i guess i should make one work first
 
-
-
+        // while (meteorRemainCount > 0)
+        // {
+        //     while (waveRemainCount > 0)
+        //     {
+        //         if (scoreInterval > 100 && currentEnemyCount = 0)
+        //         {
+        //             // begin wave spawn
+        //             // scoreInterval = 0
+        //         }
+        //     }
+        // }
 
     }
     
-    IEnumerator WaveControlMainMethod()
-    {
-        yield return new WaitForSeconds(startWait);
-        for (int i = 0; i < 5; i++) // while waves AND meteors have not been exhausted
-        {
-            if (currentScore == 2) // if score > some threshold
-            {
-                // begin spawning wave
-            }
-
-        }
-    }
-
     IEnumerator Placeholder()
     {
         yield return new WaitForSeconds(0);
+    }
+
+}
+
+// why cant i call this private
+public class WaveReader
+{
+    public int wavesTotal;
+    public Component[] enemyCountCarrier;
+    public int[] enemiesEachWave;
+    private GameObject waveContainer;
+
+    // this is a constructor. note there's no type (e.g. void).
+    // when public and same name as class, allows passing of inputs.   
+    public WaveReader(GameObject waveContainer)
+    {
+        this.waveContainer = waveContainer;
+    }
+
+    public void test()
+    {
+        // get number of waves
+        wavesTotal = waveContainer.transform.childCount;
+
+        // get number of enemies on each wave
+        enemyCountCarrier = waveContainer.GetComponentsInChildren(typeof(EnemyCount), true);
+        enemiesEachWave = new int[enemyCountCarrier.Length];
+        for (int i = 0; i<enemyCountCarrier.Length; i++)
+        {
+            // Debug.Log(enemyCountCarrier[i]);
+            enemiesEachWave[i] = enemyCountCarrier[i].GetComponent<EnemyCount>().enemyCount;
+            Debug.Log(enemiesEachWave[i]);
+        }
+            
+
+
+
+
+        // get transforms for each wave's points
     }
 }
