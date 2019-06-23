ec0b7e3ec46d15392b45cb75424ea39252610455
meiyaismywaifu <rokasomee@gmail.com>
2019-06-23 16:11:45 -0700

    06_23 post - thinking about balance problems
    discovered meteorsplitter sometimes splits rather than destroys on boundary.

--- Assets/Scripts/MeteorSpawner.cs
+++ Assets/Scripts/MeteorSpawner.cs
@@ -24,6 +24,16 @@ public class MeteorSpawner : MonoBehaviour
     IEnumerator SpawnWaves()
     {
         yield return new WaitForSeconds(startWait);
+
+        // desire to control number of meteors on screen.
+        /*
+         * searching for total number of instances in-scene is probably prohibitive. so can't do true count.
+         * currently there's 4 meteors that spawn. all of them are "big", i.e. 3 shots to split. but they're all different sizes... feels like this is really bad from a recognition standpoint. i couldn't figure it out at least.
+         * currently also: setup seems to have a constant amount of points per stage with any given setting. spawner only spawns bigs, bigs always spawn 2 meds, meds always spawn 2 smalls. which is good. and since speed is constant that means number of points on screen is also constant...
+         * 
+         * is it just a balance problem? of how fast to spawn things / how many hits / etc.?
+         * 
+         */
         for (int i = 0; i < spawnCount; i++)
         {
             Vector3 spawnPosition = new Vector3(Random.Range(-spawnArea.x, spawnArea.x), spawnArea.y, spawnArea.z);
